f54bde8d4d96b13c7e5f281e58a04af9b2cf614f
Tuyet-Vy <nhuochanvu@gmail.com>
2022-09-24 02:41:36 +0700

    finish chair render layout

--- src/redux/BookingTicket/ExerciseBookingTicket.jsx
+++ src/redux/BookingTicket/ExerciseBookingTicket.jsx
@@ -1,31 +1,68 @@
-import React, { Component } from 'react';
-import './ExerciseBookingTicket.css';
-import SeatBookingInfo from './SeatBookingInfo';
+import React, { Component, Fragment } from "react";
+import "./ExerciseBookingTicket.css";
+import SeatBookingInfo from "./SeatBookingInfo";
+import danhSachGhe from "../../Data/danhSachGhe.json";
+import SeatsRow from "./SeatsRow";
 
 export default class ExerciseBookingTicket extends Component {
+
+  renderSeatRow = () => {
+    return danhSachGhe.map((seatRow, index) => {
+      return <div key={index}>
+        <SeatsRow seatRow={seatRow} rowNumber={index}/>
+      </div>
+    })
+  }
+
   render() {
     return (
-      <div className='bookingMovie' style={{position:'fixed', width:'100%', height:'100%', backgroundImage:'url(./img/bgmovie.jpg)', backgroundSize:'100%'}}>
-        <div style={{position:'fixed', width:'100%', height:'100%', backgroundColor:'rgba(0,0,0,0.7)'}}>
-          <div className='container'>
-            <div className='row mt-3'>
-              <div className='col-8 text-center'>
-                <h1 className='text-warning'>ĐẶT VÉ XEM PHIM CYBERLEARN.VN</h1>
-                <h3 className='text-light mt-5'>Màn Hình</h3>
-                <div className='mt-3' style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
-                  <div className='screen'>
+      <div
+        className="bookingMovie"
+        style={{
+          position: "fixed",
+          width: "100%",
+          height: "100%",
+          backgroundImage: "url(./img/bgmovie.jpg)",
+          backgroundSize: "100%",
+        }}
+      >
+        <div
+          style={{
+            position: "fixed",
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.7)",
+          }}
+        >
+          <div className="container-fluid">
+            <div className="row mt-2">
+              <div className="col-8 text-center">
+                <h1 className="text-warning">ĐẶT VÉ XEM PHIM CYBERLEARN.VN</h1>
+                <h3 className="text-light mt-3">Màn Hình</h3>
+                <div
+                  className="mt-3"
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                  }}
+                >
+                  <div className="screen">
+                  </div>
+      
                 </div>
-                  
-                </div>
+                {this.renderSeatRow()}
+                
+                
               </div>
-              <div className='col-4 mt-3'>
-                <h3 className='text-light pb-5'>DANH SÁCH GHẾ BẠN CHỌN</h3>
-                <SeatBookingInfo/>
+              <div className="col-4 mt-3">
+                <h3 className="text-light pb-5">DANH SÁCH GHẾ BẠN CHỌN</h3>
+                <SeatBookingInfo />
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
--- src/redux/BookingTicket/SeatsRow.jsx
+++ src/redux/BookingTicket/SeatsRow.jsx
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
 
 export default class SeatsRow extends Component {
+
+  renderGhe = () => {
+    return this.props.seatRow.danhSachGhe.map((chairNumber, index) => {
+      let cssBookedChair = '';
+      let disabled = false;
+      if(chairNumber.daDat){
+        cssBookedChair = 'gheDuocChon';
+        disabled = true;
+      }
+
+      return <button disabled={disabled} className={`ghe ${cssBookedChair}`} key={index}>
+        {chairNumber.soGhe}
+      </button>
+    })
+  }
+
+  renderFirstRow = () => {
+    return this.props.seatRow.danhSachGhe.map((hang, index) => {
+      return <button className='rowNumber'>
+        {hang.soGhe}
+    </button>
+    }) 
+  }
+
+  renderSeatRow = () => {
+    if(this.props.rowNumber === 0){
+      return <div style={{marginLeft:'30px'}}>
+        {this.props.seatRow.hang} {this.renderFirstRow()}
+      </div>
+    } else{
+      return <div>
+          {this.props.seatRow.hang} {this.renderGhe()}
+        </div>
+    }
+  }
+  
   render() {
     return (
-      <div>SeatsRow</div>
+      <div className='text-light text-start mt-1' style={{fontSize:'30px'}}>
+        {this.renderSeatRow()}
+      </div>
     )
   }
 }
