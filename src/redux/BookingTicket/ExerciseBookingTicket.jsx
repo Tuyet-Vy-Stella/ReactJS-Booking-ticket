a16aa6efe59c63864d18fba076702e485bac30b1
Tuyet-Vy <nhuochanvu@gmail.com>
2022-09-24 00:45:05 +0700

    complete layout

--- src/redux/BookingTicket/ExerciseBookingTicket.jsx
+++ src/redux/BookingTicket/ExerciseBookingTicket.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import './ExerciseBookingTicket.css';
+import SeatBookingInfo from './SeatBookingInfo';
 
 export default class ExerciseBookingTicket extends Component {
   render() {
     return (
-      <div>ExerciseBookingTicket</div>
+      <div className='bookingMovie' style={{position:'fixed', width:'100%', height:'100%', backgroundImage:'url(./img/bgmovie.jpg)', backgroundSize:'100%'}}>
+        <div style={{position:'fixed', width:'100%', height:'100%', backgroundColor:'rgba(0,0,0,0.7)'}}>
+          <div className='container'>
+            <div className='row mt-3'>
+              <div className='col-8 text-center'>
+                <h1 className='text-warning'>ĐẶT VÉ XEM PHIM CYBERLEARN.VN</h1>
+                <h3 className='text-light mt-5'>Màn Hình</h3>
+                <div className='mt-3' style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
+                  <div className='screen'>
+                </div>
+                  
+                </div>
+              </div>
+              <div className='col-4 mt-3'>
+                <h3 className='text-light pb-5'>DANH SÁCH GHẾ BẠN CHỌN</h3>
+                <SeatBookingInfo/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/redux/BookingTicket/SeatBookingInfo.jsx
+++ src/redux/BookingTicket/SeatBookingInfo.jsx
@@ -1,9 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class SeatBookingInfo extends Component {
   render() {
     return (
-      <div>SeatBookingInfo</div>
-    )
+      <div>
+        <div className="mt-5">
+          <button className="gheDuocChon"></button>
+          <span className="text-light mx-3" style={{ fontSize: "30px" }}>
+            Ghế đã đặt
+          </span>
+          <br />
+          <button className="gheDangChon"></button>
+          <span className="text-light mx-3" style={{ fontSize: "30px" }}>
+            Ghế đang đặt
+          </span>
+          <br />
+          <button className="ghe" style={{ marginLeft: "0px" }}></button>
+          <span className="text-light mx-3" style={{ fontSize: "30px" }}>
+            Ghế chưa đặt
+          </span>
+        </div>
+
+        <div className="mt-4">
+          <table className="table" border="2">
+            <thead>
+              <tr className="text-light" style={{fontSize:'25px'}}>
+                <th style={{borderWidth:'1px', padding:'0.75rem'}}>Số ghế</th>
+                <th style={{borderWidth:'1px', padding:'0.75rem'}}>Giá</th>
+                <th style={{borderWidth:'1px', padding:'0.75rem'}}></th>
+              </tr>
+            </thead>
+            <tbody>
+            
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
   }
 }
